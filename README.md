3a0b0cc9d394dd097cf45e97520ed67ec35b9461
Connor S Patsel <cpatsel@ada8.computing.clemson.edu>
2018-10-22 14:25:07 -0400

    added feeder script

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ imgdl.py requires selenium:
 
 `` pip install selenium ``
 
-Windows version of chromedriver.exe is included.
+
+feeder.py should be placed in the folder containing the darknet program. By default it searches for images in a folder named 'train'
+
+
+Both windows and linux versions of chrome driver are included.
--- imgdl.py
+++ imgdl.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotVisibleException
-
+from sys import platform
 
 #Proof of concept flickr downloader using selenium webdriver. (pip install selenium)
 #This assumes that the download link will be available which is not always the case.
 #Should be further extended and automated to be able to dynamically take photo ids and download them.
 
-browser = webdriver.Chrome() #Set the browser to use. In this case use the chromedriver.exe included.
 
+#Set chromedriver path. Use exe if windows, binary if linux
+driverPath = "./chromedriver.exe"
+if platform.startswith('linux'):
+    driverPath = "./chromedriver"
+	
+
+browser = webdriver.Chrome(executable_path=driverPath) #Set the browser to use. 
 
 print "Loading page"
 browser.get('https://flickr.com/photo.gne?id=2177060015') #Navigate to the page by using the link that accepts only photo ids.
@@ -21,3 +27,5 @@ downloadText = "Download the Large 1024 size of this photo" #This is the text on
 
 browser.find_element_by_link_text(downloadText).click() #Click the link. Unless otherwise configured, chrome will save 
                                                         #this link automatically into the downloads folder.
+
+#Todo close the browser with browser.quit(). Doing so here quits the browser before the download is complete.
